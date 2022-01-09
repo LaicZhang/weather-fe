ff125ee2bf4ddd905d5b40b7fd6cc037ac3f6fda
laiczhang <i@laiczhang.com>
2022-01-09 16:29:11 +0800

    feat: improve the interface of login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,40 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./../components/Home.vue";
-import Welcome from "./../components/Welcome.vue";
-import Login from "./../components/Login.vue";
+import Welcome from "../views/Welcome.vue";
 
 const routes = [
-    {
-        name: "home",
-        path: '/',
-        meta:{
-            title: 'Home'
+  {
+    name: "home",
+    path: "/",
+    meta: {
+      title: "Home",
+    },
+    redirective: "/welcome",
+    component: Home,
+    children: [
+      {
+        name: "welcome",
+        path: "/welcome",
+        meta: {
+          title: "Welcome",
         },
-        redirective: '/welcome',
-        component: Home,
-        children: [
-            {
-                name: "welcome",
-                path: '/welcome',
-                meta:{
-                    title: 'Welcome'
-                },
-                component: Welcome,
-            },
-            {
-                name: "login",
-                path: '/login',
-                meta:{
-                    title: 'Login'
-                },
-                component: Login,
-            }
-        ]
-    }
-]
+        component: Welcome,
+      },
+    ],
+  },
+  {
+    name: "login",
+    path: "/login",
+    meta: {
+      title: "Login",
+    },
+    component: () => import("../views/Login.vue"),
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,104 @@
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      user: {
+        userName: "",
+        userPwd: "",
+      },
+      rules: {
+        userName: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+        ],
+        userPwd: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.userForm.validate((valid) => {
+        if (valid) {
+          this.$api.login(this.user).then((res) => {
+            this.$store.commit("saveUserInfo", res);
+            this.$router.push("/welcome");
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    goHome() {
+      this.$router.push("/welcome");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="login-wrapper">
+    <div class="modal">
+      <el-form ref="userForm" :model="user" status-icon :rules="rules">
+        <div class="title">登录</div>
+        <el-form-item prop="userName">
+          <el-input
+            type="text"
+            label="用户名"
+            prefix-icon="el-icon-user"
+            v-model="user.userName"
+          />
+        </el-form-item>
+        <el-form-item prop="userPwd">
+          <el-input
+            type="password"
+            label="密码"
+            prefix-icon="el-icon-view"
+            v-model="user.userPwd"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="btn-login" @click="login"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9fcff;
+  width: 100vw;
+  height: 100vh;
+  .modal {
+    width: 500px;
+    padding: 50px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0px 0px 10px 3px #c7c9cb4d;
+    .title {
+      font-size: 50px;
+      line-height: 1.5;
+      text-align: center;
+      margin-bottom: 30px;
+    }
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
+</style>
