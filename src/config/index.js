f04b350904950825b2b88c6d62e9ad7aeff8ff1a
laiczhang <i@laiczhang.com>
2022-01-09 19:43:05 +0800

    fix: fixed the local server api's path

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const env = import.meta.env.MODE || 'development';
 const EnvConfig = {
     development: {
-        baseApi: '/',
+        baseApi: '/api',
         mockApi: 'https://www.fastmock.site/mock/47e14f20fa22e4e2f62621aca6bd9d01/api',
     },
     production: {
