2a58570ad51024e37a5a8e56e09d5d2b4a039008
laiczhang <i@laiczhang.com>
2022-01-09 20:23:07 +0800

    config: add alias configure

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-      },
+  resolve: {
+    alias:{
+      '@': path.resolve( __dirname, './src' )
+    }
+  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import '@/assets/style/base.scss';`
+  //     }
+  //   }
+  // },
+  server:{
+    host:'localhost',
+    port:8080,
+    proxy:{
+      "/api":{
+        target:"http://localhost:3000"
+      }
     }
   },
   plugins: [vue()]
