da590b05b2fc2e69a6be15337d15e61d6b0e09f9
laiczhang <i@laiczhang.com>
2022-01-10 14:39:00 +0800

    feat: update router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,54 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "./../components/Home.vue";
-import Welcome from "../views/Welcome.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/components/Home.vue'
 
 const routes = [
-  {
-    name: "home",
-    path: "/",
-    meta: {
-      title: "Home",
-    },
-    redirective: "/welcome",
-    component: Home,
-    children: [
-      {
-        name: "welcome",
-        path: "/welcome",
+    {
+        name: 'home',
+        path: '/',
         meta: {
-          title: "Welcome",
+            title: '首页'
         },
-        component: Welcome,
-      },
-    ],
-  },
-  {
-    name: "login",
-    path: "/login",
-    meta: {
-      title: "Login",
+        component: Home,
+        redirect: '/welcome',
+        children: [
+            {
+                name: 'welcome',
+                path: '/welcome',
+                meta: {
+                    title: '欢迎体验Vue3全栈课程'
+                },
+                component: () => import('@/views/Welcome.vue')
+            },
+            {
+                name: 'user',
+                path: '/system/user',
+                meta: {
+                    title: '用户管理'
+                },
+                component: () => import('@/views/User.vue')
+            },
+            {
+                name: 'menu',
+                path: '/system/menu',
+                meta: {
+                    title: '菜单管理'
+                },
+                component: () => import('@/views/Menu.vue')
+            }
+        ]
     },
-    component: () => import("../views/Login.vue"),
-  },
-];
-
+    {
+        name: 'login',
+        path: '/login',
+        meta: {
+            title: '登录'
+        },
+        component: () => import('./../views/Login.vue')
+    }
+]
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+    history: createWebHashHistory(),
+    routes
+})
 
-export default router;
+export default router
