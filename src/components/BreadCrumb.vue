e7ea8e0bf2990ed19cdf460bd68040c5fc94efa3
laiczhang <i@laiczhang.com>
2022-01-10 14:39:33 +0800

    feat: add BreadCrumb.vue

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-breadcrumb-item v-for="(item, index) in breadList" :key="item.path">
+      <router-link to="/welcome" v-if="index == 0">{{
+        item.meta.title
+      }}</router-link>
+      <span v-else>{{ item.meta.title }}</span>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+<script>
+export default {
+  name: "BreadCrumb",
+  computed: {
+    breadList() {
+      return this.$route.matched;
+    },
+  },
+};
+</script>
