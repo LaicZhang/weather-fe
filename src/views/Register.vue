5c1e20ad94e01443bcdacf8571caa669305ede4b
laiczhang <i@laiczhang.com>
2022-01-10 14:54:57 +0800

    feat: add register.vue

---
+++ src/views/Register.vue
@@ -0,0 +1,115 @@
+<script>
+export default {
+  name: "register",
+  data() {
+    return {
+      user: {
+        userName: "",
+        userPwd: "",
+        userEmail: "",
+      },
+      rules: {
+        userName: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+        ],
+        userEmail: [
+          {
+            required: true,
+            message: "请输入邮箱",
+            trigger: "blur",
+          },
+        ],
+        userPwd: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.user);
+      this.$refs.userForm.validate((valid) => {
+        if (valid) {
+          this.$api.login(this.user).then((res) => {
+            this.$store.commit("saveUserInfo", res);
+            this.$router.push("/welcome");
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    goHome() {
+      this.$router.push("/welcome");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="login-wrapper">
+    <div class="modal">
+      <el-form ref="userForm" :model="user" status-icon :rules="rules">
+        <div class="title">登录</div>
+        <el-form-item label="账号" prop="userName">
+          <el-input
+            type="text"
+            v-model="user.userName"
+          />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="userEmail">
+          <el-input
+            type="password"
+            v-model="user.userEmail"
+          />
+        </el-form-item>
+        <el-form-item label="密码" prop="userPwd">
+          <el-input
+            type="password"
+            v-model="user.userPwd"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="btn-login" @click="login"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9fcff;
+  width: 100vw;
+  height: 100vh;
+  .modal {
+    width: 500px;
+    padding: 50px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0px 0px 10px 3px #c7c9cb4d;
+    .title {
+      font-size: 50px;
+      line-height: 1.5;
+      text-align: center;
+      margin-bottom: 30px;
+    }
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
+</style>
