16ab75c56a2312e373857f0281044adca7bca64b
laiczhang <i@laiczhang.com>
2022-01-13 16:09:12 +0800

    feat: add dropdown component

---
+++ src/components/dropdown/c-user-dropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-dropdown @command="onLoginOut">
+    <span class="el-dropdown-link user-dropdown-title">
+      {{ userInfo.userName }}
+      <i class="el-icon-arrow-down"></i>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item command="email">{{
+          userInfo.userEmail
+        }}</el-dropdown-item>
+        <el-dropdown-item command="out">退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+<script>
+export default {
+  name: "CUserDropdown",
+  props: {
+    userInfo: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  methods: {
+    onLoginOut(command) {
+      if (command === "out") {
+        this.$store.commit("setUserInfo", "");
+        this.$store.commit('setActionList',[])
+        this.$store.commit('setMenuList',[])
+        this.$router.push({ name: "login" });
+      }
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.user-dropdown-title {
+  color: $color-active;
+  margin-left: 20px;
+}
+</style>
