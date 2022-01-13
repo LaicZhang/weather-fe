32885e8e95be2ae94423a2e53de92229e0a7ad6e
laiczhang <i@laiczhang.com>
2022-01-13 16:08:48 +0800

    fix: fix api error about main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import "element-plus/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import api from "./api";
 import request from "./util/request";
 import storage from "./util/storage";
 import hasAction from './directive/has-action'
@@ -13,7 +14,7 @@ import hasAction from './directive/has-action'
 const app = createApp(App)
 app.config.globalProperties.$request = request
 app.config.globalProperties.$storage = storage
-app.config.globalProperties.$api = api
+// app.config.globalProperties.$api = api
 app.config.globalProperties.$store = store
 
 app.directive('has',hasAction)
