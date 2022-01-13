c82ba7d5bda5b5114f5e15b685ee1e3bb0c4925c
laiczhang <i@laiczhang.com>
2022-01-13 15:28:42 +0800

    feat: update api/index.js code format

--- src/api/index.js
+++ src/api/index.js
@@ -1,75 +1,76 @@
-/**
- * api管理
- */
- import request from './../utils/request'
- export default {
-     login(params) {
-         return request({
-             url: '/users/login',
-             method: 'post',
-             data: params,
-         })
-     },
-     noticeCount(params) {
-         return request({
-             url: '/leave/count',
-             method: 'get',
-             data: {},
-             mock: true
-         })
-     },
-     getMenuList(params) {
-         return request({
-             url: '/menu/list',
-             method: 'get',
-             data: params,
-             mock: false
-         })
-     },
-     getUserList(params) {
-         return request({
-             url: '/users/list',
-             method: 'get',
-             data: params
-         })
-     },
-     userDel(params) {
-         return request({
-             url: '/users/delete',
-             method: 'post',
-             data: params
-         })
-     },
-     getRoleList() {
-         return request({
-             url: '/roles/allList',
-             method: 'get',
-             data: {},
-             mock: true
-         })
-     },
-     getDeptList() {
-         return request({
-             url: '/dept/list',
-             method: 'get',
-             data: {},
-             mock: true
-         })
-     },
-     userSubmit(params) {
-         return request({
-             url: '/users/operate',
-             method: 'post',
-             data: params,
-             mock: false
-         })
-     },
-     menuSubmit(params) {
-         return request({
-             url: '/menu/operate',
-             method: 'post',
-             data: params,
-             mock: false
-         })
-     }
- }
+import request from "@/util/request";
+
+export const loginApi = (data) => {
+  return request.post("/users/login", data, { mock: false });
+};
+
+export const menuListApi = (data = {}) => {
+  return request.get("/menu/list", data, { mock: false });
+};
+
+// 获取当前角色的权限
+export const menuPermissionApi = (data = {}) => {
+  return request.get("/users/getPermissionList", data, { mock: false });
+};
+
+export const menuOperateApi = (data = {}) => {
+  return request.post("/menu/operate", data, { mock: false });
+};
+
+export const leaveCountApi = (data = {}) => {
+  return request.get("/leave/count", data,  { mock: false });
+};
+export const leaveListApi = (data={})=>{
+  return request.get("/leave/list", data,  { mock: false });
+}
+export const leaveOperateApi = (data={})=>{
+  return request.post("/leave/operate", data,  { mock: false });
+}
+// 审核
+export const approveOperateApi = (data={})=>{
+  return request.post("/leave/approve", data,  { mock: false });
+}
+export const userListApi = (data = {}) => {
+  return request.get("/users/list", data, { mock: false });
+};
+
+// 获取负责人
+export const userAllListApi = (data = {}) => {
+  return request.get("/users/all/list", data, { mock: false });
+};
+
+// 删除单个或多个 `userIds:[userId,userId...]`
+export const deleteUserApi = (data = {}) => {
+  return request.post("/users/delete", data, { mock: false });
+};
+// 获取角色名称列表
+export const rolesAllListApi = (data = {}) => {
+  return request.get("/roles/allList", data, { mock: false });
+};
+// 获取角色列表
+export const roleListApi = (data = {}) => {
+  return request.get("/roles/list", data, { mock: false });
+};
+
+export const roleOperateApi = (data = {}) => {
+  return request.post("/roles/operate", data, { mock: false });
+};
+
+export const rolePermissionApi = (data = {}) => {
+  return request.post("/roles/update/permission", data, { mock: false });
+};
+
+export const deptListApi = (data = {}) => {
+  return request.get("/dept/list", data, { mock: false });
+};
+export const deptOperateApi = (data = {}) => {
+  return request.post("/dept/operate", data, { mock: false });
+};
+export const addUserApi = (data = {}) => {
+  data.action = "add";
+  return request.post("/users/operate", data, { mock: false });
+};
+export const editUserApi = (data = {}) => {
+  data.action = "edit";
+  return request.post("/users/operate", data, { mock: false });
+};
