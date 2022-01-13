ccbbbb600a3add940ebeb0e152d474daca523c1e
laiczhang <i@laiczhang.com>
2022-01-13 16:07:36 +0800

    feat: add menu componet

---
+++ src/components/menu/c-menu.vue
@@ -0,0 +1,90 @@
+<template>
+  <el-menu
+  :default-active="activePath"
+    class="menu-wrap"
+    :collapse="isCollapse"
+    background-color="#071321"
+    text-color="#ffffff"
+    active-text-color="#409eff"
+    router
+    ref="menuRef"
+  >
+    <div class="menu-logo" :class="[calcLogoClass ? 'fold' : '']">
+      <div class="menu-logo-img"></div>
+      <div class="menu-logo-title">manager</div>
+    </div>
+    <c-tree-menu :menus="menus"></c-tree-menu>
+  </el-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import CTreeMenu from "./c-tree-menu.vue";
+
+export default defineComponent({
+  name: "CMenu",
+  props: {
+    menus: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+  components: { CTreeMenu },
+  data() {
+    return {
+      isCollapse: false,
+      activePath: ''
+    };
+  },
+  mounted() {
+    this.activePath = this.$route.path
+    // try {
+    //   const path = "/" + this.$route.path.split("/").filter((item) => item)[0];
+    //   this.$refs.menuRef.open(path);
+    // } catch (error) {}
+  },
+  computed: {
+    calcLogoClass() {
+      return this.isCollapse;
+    },
+  },
+  methods: {
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
+      return this.isCollapse;
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+.menu-wrap {
+  height: 100%;
+  overflow-y: auto;
+  border: none;
+  transition: all 200ms;
+  .menu-logo {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &.fold {
+      .menu-logo-title {
+        display: none;
+      }
+    }
+    .menu-logo-img {
+      width: 40px;
+      height: 40px;
+      background: url("../../assets/logo.png") no-repeat center/cover;
+    }
+    .menu-logo-title {
+      font-size: 20px;
+      margin-left: 10px;
+      color: white;
+    }
+  }
+}
+</style>
+
---
+++ src/components/menu/c-tree-menu.vue
@@ -0,0 +1,42 @@
+<template>
+  <template v-for="(menu, index) in menus">
+    <el-sub-menu
+      :index="menu.path"
+      v-if="
+        menu.children &&
+        menu.children.length > 0 &&
+        menu.children[0].menuType == '1'
+      "
+    >
+      <template #title>
+        <i :class="menu.icon"></i>
+        <span>{{ menu.menuName }}</span>
+      </template>
+      <c-tree-menu :menus="menu.children"></c-tree-menu>
+    </el-sub-menu>
+    <el-menu-item v-else-if="menu.menuType !== '2'" :index="menu.path" >{{
+      menu.menuName
+    }}</el-menu-item>
+  </template>
+  <!-- <div class="menu-item-wrap"> -->
+  <!-- </div> -->
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CTreeMenu",
+  props: {
+    menus: {  
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+  components: {},
+});
+</script>
+<style lang="scss" scoped>
+</style>
