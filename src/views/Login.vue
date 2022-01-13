ac4980b80f6e0a96c088843c5db1662dcada6f45
laiczhang <i@laiczhang.com>
2022-01-13 15:39:24 +0800

    feat: add login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,99 +1,118 @@
+
+<template>
+  <div class="login-page">
+    <el-form
+      class="login-form"
+      ref="userFormRef"
+      :model="userForm"
+      :rules="userRules"
+      status-icon
+    >
+      <div class="login-title">后台系统</div>
+      <el-form-item prop="userName">
+        <el-input
+          v-model="userForm.userName"
+          type="text"
+          placeholder="请输入账号"
+          prefix-icon="el-icon-user"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="userPwd">
+        <el-input
+          v-model="userForm.userPwd"
+          type="text"
+          placeholder="请输入密码"
+          prefix-icon="el-icon-lock"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="userFromCommit" type="primary" class="login-submit"
+          >登录</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
 <script>
-export default {
-  name: "login",
-  data() {
-    return {
-      user: {
-        userName: "",
-        userPwd: "",
-      },
-      rules: {
-        userName: [
-          {
-            required: true,
-            message: "请输入用户名",
-            trigger: "blur",
-          },
-        ],
-        userPwd: [
-          {
-            required: true,
-            message: "请输入密码",
-            trigger: "blur",
-          },
-        ],
-      },
+import { defineComponent, onMounted, reactive, ref } from "vue";
+import useVuexWithRouter from "@/hooks/useVuexWithRouter";
+import { loginApi, menuPermissionApi } from "@/api";
+export default defineComponent({
+  name: "Login",
+  components: {},
+  setup() {
+    const { router, store } = useVuexWithRouter();
+    const toPageHome = () => {
+      router.push("/");
     };
-  },
-  methods: {
-    login() {
-      console.log(this.user);
-      this.$refs.userForm.validate((valid) => {
+    const userFormRef = ref(null);
+    const userForm = reactive({
+      userName: "",
+      userPwd: "",
+    });
+    const userRules = {
+      userName: [
+        {
+          required: true,
+          message: "必须填写用户名",
+          trigger: "blur",
+        },
+      ],
+      userPwd: [
+        {
+          required: true,
+          message: "必须填写密码",
+          trigger: "blur",
+        },
+      ],
+    };
+    const userFromCommit = () => {
+      userFormRef.value.validate(async (valid) => {
         if (valid) {
-          this.$api.login(this.user).then((res) => {
-            this.$store.commit("saveUserInfo", res);
-            this.$router.push("/welcome");
-          });
+          const loginInfo = await loginApi(userForm);
+          store.commit("setUserInfo", loginInfo);
+          await getMenuPermission();
+          toPageHome();
         } else {
           return false;
         }
       });
-    },
-    goHome() {
-      this.$router.push("/welcome");
-    },
+    };
+    const getMenuPermission = async () => {
+      const { menuList, actionList } = await menuPermissionApi();
+      store.commit("setActionList", actionList);
+      store.commit("setMenuList", menuList);
+    };
+    return {
+      toPageHome,
+      userFormRef,
+      userForm,
+      userRules,
+      userFromCommit,
+    };
   },
-};
+});
 </script>
 
-<template>
-  <div class="login-wrapper">
-    <div class="modal">
-      <el-form ref="userForm" :model="user" status-icon :rules="rules">
-        <div class="title">登录</div>
-        <el-form-item label="账号" prop="userName">
-          <el-input
-            type="text"
-            v-model="user.userName"
-          />
-        </el-form-item>
-        <el-form-item label="密码" prop="userPwd">
-          <el-input
-            type="password"
-            v-model="user.userPwd"
-          />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" class="btn-login" @click="login"
-            >登录</el-button
-          >
-        </el-form-item>
-      </el-form>
-    </div>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-.login-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #f9fcff;
+<style lang="scss"  scoped >
+.login-page {
   width: 100vw;
   height: 100vh;
-  .modal {
-    width: 500px;
-    padding: 50px;
-    background-color: #fff;
-    border-radius: 4px;
-    box-shadow: 0px 0px 10px 3px #c7c9cb4d;
-    .title {
-      font-size: 50px;
-      line-height: 1.5;
+  .login-form {
+    padding: 30px;
+    width: 400px;
+    height: 300px;
+    box-shadow: $shadow-light;
+    border-radius: 10px;
+    @extend .center-all;
+    text-align: center;
+    .login-title {
+      height: 60px;
+      font-size: $font-size-large;
       text-align: center;
-      margin-bottom: 30px;
     }
-    .btn-login {
+    .login-submit {
       width: 100%;
     }
   }
