eac81cafde5922e8bb8005a80f2a176dea14a553
laiczhang <i@laiczhang.com>
2022-02-02 22:39:23 +0800

    feat: load Async Routes

--- src/router/index.js
+++ src/router/index.js
@@ -86,58 +86,65 @@ const router = createRouter({
 // 加载动态路由
 // await loadAsyncRoutes();
 router.beforeEach(async (to, from, next) => {
-  // const valid = tokenValidate()
-  // if (to.path === "/login" || valid) {
-  //   next();
-  //   // await loadAsyncRoutes();
-  // } else {
-  //   next({ name: "login" });
-  // }
+  const valid = tokenValidate()
+  if (to.path === "/login" || valid) {
+    next();
+    await loadAsyncRoutes();
+  } else {
+    next({ name: "login" });
+  }
   document.title = to.meta.title;
   next();
 });
 
-// export async function loadAsyncRoutes() {
-//   const valid = tokenValidate()
-//   if (valid) {
-//     const { menuList } = await menuPermissionApi();
-//     const result = generatorRoutes(menuList);
-//     result.forEach((route) => {
-//       const path = `./../views/${route.component}.vue`;
-//       router.addRoute("home", { ...route, component: () => import(path) });
-//     });
-//   }else{
-//     router.push({name:'login'})
-//   }
-// }
+export async function loadAsyncRoutes() {
+  const valid = tokenValidate()
+  if (valid) {
+    const { menuList } = await menuPermissionApi();
+    const result = generatorRoutes(menuList);
+    result.forEach((route) => {
+      const path = `./../views/${route.component}.vue`;
+      const modules = import.meta.globalEager(`./../views/*.vue`);
+      router.addRoute("home", { 
+        ...route, 
+        component: modules[path].default
+      });
+    });
+  }else{
+    router.push({name:'login'})
+  }
+}
 
-// function generatorRoutes(menuList) {
-//   const result = [];
-//   const deep = (list) => {
-//     while (list.length) {
-//       const item = list.pop();
-//       if (item.action) {
-//         result.push({
-//           name: item.component,
-//           path: item.path,
-//           meta: { title: item.menuName },
-//           component: item.component,
-//         });
-//       }
-//       if (item.children && !item.action) {
-//         deep(item.children);
-//       }
-//     }
-//   };
-//   deep(JSON.parse(JSON.stringify(menuList)));
-//   return result;
-// }
-// function tokenValidate() {
-//   const userInfo = storage.getItem("userInfo") || {};
-//   if (userInfo.token) {
-//     return true;
-//   }
-// }
+function generatorRoutes(menuList) {
+  const result = [];
+  const deep = (list) => {
+    while (list.length) {
+      const item = list.pop();
+      if (item.action) {
+        result.push({
+          name: item.component,
+          path: item.path,
+          meta: { title: item.menuName },
+          component: item.component,
+        });
+      }
+      if (item.children && !item.action) {
+        deep(item.children);
+      }
+    }
+  };
+  deep(JSON.parse(JSON.stringify(menuList)));
+  return result;
+}
+function tokenValidate() {
+  const userInfo = storage.getItem("userInfo") || {};
+  // if (userInfo.token) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return !!userInfo.token;
+}
 window.router = router;
 
 export default router;
