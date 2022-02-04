710f053e261ea00de534784659f99ed682d6c27a
laiczhang <i@laiczhang.com>
2022-02-04 22:48:36 +0800

    style: update code style about sign page

--- src/views/Sign.vue
+++ src/views/Sign.vue
@@ -59,29 +59,31 @@ export default {
             console.log("toPageHome");
             router.push("/");
         },
-    },
-    mounted(){
-        const signUpButton = document.getElementById('signUp');
-        const signInButton = document.getElementById('signIn');
-        const container = document.getElementById('container');
+        init(){
+            const signUpButton = document.getElementById('signUp');
+            const signInButton = document.getElementById('signIn');
+            const container = document.getElementById('container');
 
-        signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
-        signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+            signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+            signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
 
-        var demoImg = document.getElementById("demo_img");
-        var demoInput = document.getElementById("demo_input");
+            var demoImg = document.getElementById("demo_img");
+            var demoInput = document.getElementById("demo_input");
 
-        //隐藏text block，显示password block
-        function hideShowPsw(){
-            if (demoInput.type == "password") {
-                demoInput.type = "text";
-                demo_img.src = "invisible.png";
-            }else {
-                demoInput.type = "password";
-                demo_img.src = "visible.png";
+            //隐藏text block，显示password block
+            function hideShowPsw(){
+                if (demoInput.type == "password") {
+                    demoInput.type = "text";
+                    demo_img.src = "invisible.png";
+                }else {
+                    demoInput.type = "password";
+                    demo_img.src = "visible.png";
+                }
             }
         }
-
+    },
+    mounted(){
+        this.init();
     }
 }
 </script>
