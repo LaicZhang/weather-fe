d4d0ffbb4798a4999790c893fb87af8e3291a3c3
laiczhang <i@laiczhang.com>
2022-02-04 19:54:50 +0800

    chore: update project config

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
       "/api":{
         target:"http://localhost:3000",
         changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   },
