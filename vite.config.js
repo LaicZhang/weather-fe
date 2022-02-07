83a83cb34ddeb5a7a110d8c89acf7b36f55406d1
laiczhang <i@laiczhang.com>
2022-02-07 20:21:29 +0800

    chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -27,7 +26,6 @@ export default defineConfig({
         target:"http://localhost:3000",
         changeOrigin: true,
         secure: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   },
