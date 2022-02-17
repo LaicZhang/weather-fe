755cc390eb53110b479a1c3a813356e3a3aab149
laiczhang <i@laiczhang.com>
2022-02-17 20:08:47 +0800

    fix: fixed that the project can't run `vite build` by https://github.com/vitejs/vite/issues/5597

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+var charsetRemoval = () => {
+    return {
+        postcssPlugin: 'internal:charset-removal',
+        AtRule: {
+            charset: atRule => {
+                if (atRule.name === 'charset') {
+                    atRule.remove()
+                }
+            }
+        }
+    }
+}
+
+module.exports = () => {
+    return {
+        plugins: [
+            //其他postcss插件
+            charsetRemoval()
+        ]
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,27 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        charset: false,
         additionalData: `
           @import '@/assets/scss/mixin.scss';
           @import '@/assets/scss/comm.scss';
           @import '@/assets/scss/variable.scss';`,
       },
+      // https://github.com/vitejs/vite/issues/5833
+      postcss:{
+        plugins:[
+          {
+            postcssPlugin: "internal:charset-removal",
+            AtRule: {
+              charset: atRule => {
+                if (atRule.name === "charset") {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
+      }
     },
   },
   server:{
@@ -28,6 +44,27 @@ export default defineConfig({
         secure: true,
       }
     }
+  },
+  optimizeDeps: {
+    include: [
+      "pinia",
+      "vue-i18n",
+      "lodash-es",
+      "@vueuse/core",
+      "@iconify/vue",
+      "element-plus/lib/locale/lang/en",
+      "element-plus/lib/locale/lang/zh-cn",
+      "vxe-table/lib/locale/lang/zh-CN",
+      "vxe-table/lib/locale/lang/en-US"
+    ],
+    exclude: ["@zougt/vite-plugin-theme-preprocessor/dist/browser-utils"]
+  },
+  build: {
+    sourcemap: false,
+    brotliSize: false,
+    // 消除打包大小超过500kb警告
+    chunkSizeWarningLimit: 2000,
+    minify: false
   },
   plugins: [vue()]
 })
