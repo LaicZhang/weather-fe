71d88dc648ed9e3d789a4ce974706992d7284a3b
laiczhang <i@laiczhang.com>
2022-02-21 21:26:04 +0800

    feat: change the user name to user image in the user dropdown component

--- src/components/dropdown/c-user-dropdown.vue
+++ src/components/dropdown/c-user-dropdown.vue
@@ -1,8 +1,16 @@
 <template>
   <el-dropdown @command="onLoginOut">
     <span class="el-dropdown-link user-dropdown-title">
-      {{ userInfo.userName }}
-      <i class="el-icon-arrow-down"></i>
+      <img
+        alt="user picture"
+        class="user-img"
+        v-lazy="{
+          src: userImage,
+          error: '../../src/assets/images/default.jpg',
+          loading: '../../src/assets/images/default.jpg',
+        }"
+      />
+      <em class="el-icon-arrow-down"></em>
     </span>
     <template #dropdown>
       <el-dropdown-menu>
@@ -15,6 +23,11 @@
 <script>
   export default {
     name: 'CUserDropdown',
+    data() {
+      return {
+        userImage: '../../src/assets/images/default.jpg',
+      }
+    },
     props: {
       userInfo: {
         type: Object,
@@ -39,5 +52,11 @@
   .user-dropdown-title {
     color: $color-active;
     margin-left: 20px;
+  }
+  .user-img {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 30px;
   }
 </style>
