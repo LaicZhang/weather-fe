aabdb579daf58d6ef51dec00e767685137cd4444
laiczhang <i@laiczhang.com>
2022-02-21 15:48:45 +0800

    style: update c-user-dropdown.vue

--- src/components/dropdown/c-user-dropdown.vue
+++ src/components/dropdown/c-user-dropdown.vue
@@ -6,40 +6,38 @@
     </span>
     <template #dropdown>
       <el-dropdown-menu>
-        <el-dropdown-item command="email">{{
-          userInfo.userEmail
-        }}</el-dropdown-item>
+        <el-dropdown-item command="email">{{ userInfo.userEmail }}</el-dropdown-item>
         <el-dropdown-item command="out">退出</el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
 </template>
 <script>
-export default {
-  name: "CUserDropdown",
-  props: {
-    userInfo: {
-      type: Object,
-      default: () => {
-        return {};
+  export default {
+    name: 'CUserDropdown',
+    props: {
+      userInfo: {
+        type: Object,
+        default: () => {
+          return {};
+        },
       },
     },
-  },
-  methods: {
-    onLoginOut(command) {
-      if (command === "out") {
-        this.$store.commit("setUserInfo", "");
-        this.$store.commit('setActionList',[])
-        this.$store.commit('setMenuList',[])
-        this.$router.push({ name: "login" });
-      }
+    methods: {
+      onLoginOut(command) {
+        if (command === 'out') {
+          this.$store.commit('setUserInfo', '');
+          this.$store.commit('setActionList', []);
+          this.$store.commit('setMenuList', []);
+          this.$router.push({ name: 'login' });
+        }
+      },
     },
-  },
-};
+  };
 </script>
 <style scoped lang="scss">
-.user-dropdown-title {
-  color: $color-active;
-  margin-left: 20px;
-}
+  .user-dropdown-title {
+    color: $color-active;
+    margin-left: 20px;
+  }
 </style>
