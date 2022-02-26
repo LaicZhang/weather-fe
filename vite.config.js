3fa6de968b6cbd48f5dbf23dec99e47a92284592
laiczhang <i@laiczhang.com>
2022-02-26 19:53:26 +0800

    chore: update server. port

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
     port:8000,
     proxy:{
       "/api":{
-        target:"http://localhost:3000",
+        target:"http://localhost:9000",
         changeOrigin: true,
         secure: true,
       }
