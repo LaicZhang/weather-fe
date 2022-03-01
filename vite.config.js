92cf4fba8eb61747347f63759f185e2042e39bb5
laiczhang <i@laiczhang.com>
2022-03-01 22:10:28 +0800

    fix: fix that process.env.NODE_ENV is undefined

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 let server={}
-if(process.env?.NODE_ENV !== 'development'){
+console.log('process.env.NODE_ENV',process.env.NODE_ENV)
+if(process.env.NODE_ENV !== 'development'){
   server={
     host:'0.0.0.0',
     proxy:{
