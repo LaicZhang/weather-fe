8bc5b24cb127a85dd7cfcab5bbcc8935512e54b7
laiczhang <i@laiczhang.com>
2022-03-01 11:29:29 +0800

    style: delete the unused apis

--- src/api/index.js
+++ src/api/index.js
@@ -15,16 +15,10 @@ export const menuPermissionApi = (data = {}) => {
   return request.get("/auth/getPermissionList", data, { mock: false });
 };
 
-export const menuListApi = (data = {}) => {
-  return request.get("/menu/list", data, { mock: false });
-};
 export const getDictApi = (item='',data = {}) => {
   return request.get("/dict?item="+item, data, { mock: false });
 };
 
-export const menuOperateApi = (data = {}) => {
-  return request.post("/menu/operate", data, { mock: false });
-};
 // 通知管理
 export const addNoticeApi = (data = {}) => {
   return request.post("/notices/add", data, { mock: false });
@@ -62,30 +56,31 @@ export const pusherListApi = (data = {}) => {
   return request.get("/pushers/list", data, { mock: false });
 };
 
-export const approveOperateApi = (data={})=>{
-  return request.post("/leave/approve", data,  { mock: false });
-}
-export const leaveCountApi = (data = {}) => {
-  return request.get("/leave/count", data, { mock: false });
-};
+// 用户管理
 export const userListApi = (data = {}) => {
   return request.get("/users/list", data, { mock: false });
 };
-
-// 获取负责人
 export const userAllListApi = (data = {}) => {
   return request.get("/users/all/list", data, { mock: false });
+};
+export const addUserApi = (data = {}) => {
+  data.action = "add";
+  return request.post("/users/operate", data, { mock: false });
+};
+export const editUserApi = (data = {}) => {
+  data.action = "edit";
+  return request.post("/users/operate", data, { mock: false });
 };
 
 // 删除单个或多个 `userIds:[userId,userId...]`
 export const deleteUserApi = (data = {}) => {
   return request.post("/users/delete", data, { mock: false });
 };
-// 获取角色名称列表
+
 export const rolesAllListApi = (data = {}) => {
   return request.get("/roles/allList", data, { mock: false });
 };
-// 获取角色列表
+
 export const roleListApi = (data = {}) => {
   return request.get("/roles/list", data, { mock: false });
 };
@@ -98,17 +93,3 @@ export const rolePermissionApi = (data = {}) => {
   return request.post("/roles/update/permission", data, { mock: false });
 };
 
-export const deptListApi = (data = {}) => {
-  return request.get("/dept/list", data, { mock: false });
-};
-export const deptOperateApi = (data = {}) => {
-  return request.post("/dept/operate", data, { mock: false });
-};
-export const addUserApi = (data = {}) => {
-  data.action = "add";
-  return request.post("/users/operate", data, { mock: false });
-};
-export const editUserApi = (data = {}) => {
-  data.action = "edit";
-  return request.post("/users/operate", data, { mock: false });
-};
