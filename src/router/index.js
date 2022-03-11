6c92f11b2cb994ba61e9bc6d5dff374557b0f86a
laiczhang <i@laiczhang.com>
2022-03-11 15:37:42 +0800

    fix: fix 404 error for the history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home";
 import Welcome from "@/views/Welcome";
 import User from "@/views/User";
@@ -108,7 +108,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 // 加载动态路由
