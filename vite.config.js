d4a42c987299d3f5ff90046fb9b6a50f90a2c387
laiczhang <i@laiczhang.com>
2022-03-12 14:13:57 +0800

    fix: fix that Rollup failed to resolve import "element-plus/es/locale/lang/zh-cn" from "src/main.js".

--- vite.config.js
+++ vite.config.js
@@ -77,7 +77,7 @@ export default defineConfig({
       '@vueuse/core',
       //     "@iconify/vue",
       //     "element-plus/lib/locale/lang/en",
-      'element-plus/lib/locale/lang/zh-cn',
+      'element-plus/es/locale/lang/zh-cn',
       //     "vxe-table/lib/locale/lang/zh-CN",
       //     "vxe-table/lib/locale/lang/en-US"
     ],
