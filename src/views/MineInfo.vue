cf61558b0624f8692d80b4ed5a4921092cb1b961
laiczhang <i@laiczhang.com>
2022-03-24 12:44:14 +0800

    feat: add PushDeer to pusher methods

--- src/views/MineInfo.vue
+++ src/views/MineInfo.vue
@@ -71,6 +71,7 @@
         <el-form-item>
           <el-checkbox v-model="pusherConfigForm.useEmail" label="邮箱" border />
           <el-checkbox v-model="pusherConfigForm.useSms" label="短信" border />
+          <el-checkbox v-model="pusherConfigForm.usePushDeer" label="PushDeer" border />
         </el-form-item>
         <el-form-item>
           <el-checkbox v-model="pusherConfigForm.useFeiShu" label="飞书" border />
@@ -87,6 +88,12 @@
         </el-form-item>
         <el-form-item v-if="pusherConfigForm.useSms" label="电话号码">
           <el-input v-model="pusherConfigForm.mobile" placeholder="暂不支持国内号码" />
+        </el-form-item>
+        <el-form-item v-if="pusherConfigForm.usePushDeer">
+          <el-button type="text" @click="pushDeerDialogVisible=true">
+            pushkey获取方法
+          </el-button>
+          <el-input v-model="pusherConfigForm.pushkey" placeholder="" />
         </el-form-item>
         <el-form-item v-if="pusherConfigForm.useFeiShu">
           <el-button type="text" @click="feishuDialogVisible=true">
@@ -165,6 +172,53 @@
       </span>
     </template>
   </el-dialog>
+  <el-dialog v-model="pushDeerDialogVisible" title="Tips" width="40%">
+    <p>iOS14+</p>
+    <p>苹果手机（iOS 14+）用系统摄像头扫描下方二维码即可拉起轻应用，亦可在苹果商店搜索「PushDeer」安装</p>
+    <p>(注意：这里不要安装PushDeer自架版)</p>
+    <el-image
+      alt="the ios pusherdeer install image"
+      src="https://cdn.zyha.cn/img/pushdeer-resize.png"
+      style="width:10vw"
+      lazy
+    />
+    <p>MacOS 11+</p>
+    <p>PushDeer有Mac客户端，亦支持推送。可在Mac应用商店中搜索「PushDeer」安装。</p>
+    <p>Android</p>
+    <p>
+      快应用尚在开发，可下载并安装Android测试版APP(
+      <a style="color:blue" href="https://github.com/easychen/pushdeer/releases/tag/android1.0alpha">GitHub</a>
+      |<a style="color:blue" href="https://gitee.com/easychen/pushdeer/releases/android1.0alpha">Gitee</a>)。
+    </p>
+    <p>获取key</p>
+    <p>
+      <ol>
+        <li>
+          1. 通过apple账号（或微信账号·仅Android版支持）登录
+        </li>
+        <li>
+          2. 切换到「设备」标签页，点击右上角的加号，注册当前设备
+        </li>
+        <li>
+          3. 切换到「Key」标签页，点击右上角的加号，创建一个Key
+        </li>
+        <li>
+          4. 将获取的key填入输入框
+        </li>
+        <li>
+          5. 注意注册设备用到了device token，应用一旦重装，device token会变，所以需要重新注册一次。
+        </li>
+      </ol>
+    </p>
+    <!-- 通过访问后边的URL即可推送内容： -->
+    <!-- <p>https://api2.pushdeer.com/message/push?pushkey=key&text=要发送的内容</p> -->
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="pushDeerDialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="pushDeerDialogVisible = false">Confirm</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
@@ -197,6 +251,7 @@ const wecomDialogVisible = ref(false)
 const dingtalkDialogVisible = ref(false)
 const feishuDialogVisible = ref(false)
 const serverchanDialogVisible = ref(false)
+const pushDeerDialogVisible = ref(false)
 const isVisitor = ref(true)
 const uploadData = ref({
   userId: userInfo.userId,
