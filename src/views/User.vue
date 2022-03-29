e2c8c410d10edfb775a1c0edc1b4601d65bd0bd5
laiczhang <i@laiczhang.com>
2022-03-29 22:55:54 +0800

    feat: add check rules for the adding user form

--- src/views/User.vue
+++ src/views/User.vue
@@ -118,7 +118,7 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import {
   defineComponent,
   getCurrentInstance,
@@ -132,6 +132,7 @@ import { ElMessage, ElMessageBox } from 'element-plus'
 import {
   addUserApi,
   changePasswordApi,
+  checkRepeatApi,
   deleteUserApi,
   editUserApi,
   rolesAllListApi,
@@ -139,275 +140,277 @@ import {
   userListApi,
 } from '../api'
 import util from '../util/utils'
-export default defineComponent({
-  name: 'User',
-  components: {},
-  setup() {
-    const { proxy } = getCurrentInstance()
-    // 属性
-    const userFrom = reactive({
-      userId: '',
-      userName: '',
-      state: 1,
-    })
-    const pager = reactive({
-      pageNum: 1,
-      pageSize: 10,
-      total: 0,
-    })
-    const userColumns = [
-      { prop: 'userId', label: '用户ID' },
-      { prop: 'userName', label: '用户名' },
-      {
-        prop: 'sex',
-        label: '性别',
-        formatter(row, column, cellValue) {
-          return { 0: '女', 1: '男' }[cellValue]
-        },
-      },
-      { prop: 'userEmail', label: '邮箱' },
-      { prop: 'mobile', label: '手机号' },
-      {
-        prop: 'role',
-        label: '角色',
-        formatter(row, column, cellValue) {
-          return { 0: '管理员', 1: '普通用户', 2: '游客' }[cellValue]
-        },
-      },
-      {
-        prop: 'state',
-        label: '状态',
-        formatter(row, column, cellValue) {
-          return { 0: '已注销', 1: '正常' }[cellValue]
-        },
-      },
-      {
-        prop: 'createTime',
-        label: '注册时间',
-        formatter(row, column, cellValue) {
-          return util.formateDate(new Date(cellValue))
-        },
-      },
-      {
-        prop: 'lastLoginTime',
-        label: '最后登录',
-        formatter(row, column, cellValue) {
-          return util.formateDate(new Date(cellValue))
-        },
-      },
-    ]
-    const isEdit = ref(false)
-    const userList = ref([])
-    const userSelects = ref([])
-    const addDialog = ref(false)
-    const deleteDialog = ref(false)
-    const addUserFrom = reactive({})
-    const roleList = ref([])
-    const deptList = ref([])
-    const addUserFromRules = {
-      userName: [{
-        required: true,
-        message: '必须填写用户名',
-        trigger: 'blur',
-      }, {
-        min: 3,
-        max: 18,
-        message: '长度在 3 到 18 个字符',
-        trigger: 'blur',
-      }],
-      userEmail: [{
-        required: true,
-        message: '必须填写邮箱',
-        trigger: 'blur',
-      }, {
-        type: 'email',
-        message: '邮箱格式不正确',
-        trigger: 'blur',
-      }],
-      mobile: [{
-        pattern: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
-        message: '手机号格式错误',
-        trigger: 'blur',
-      }, {
-        required: true,
-        message: '必须填写手机号',
-        trigger: 'blur',
-      }, {
-        type: 'number',
-        message: '手机号格式错误',
-        trigger: 'blur',
-      }, {
-        min: 11,
-        max: 11,
-        message: '手机号长度错误',
-        trigger: 'blur',
-      }],
-    }
-    // api
-    const getUserList = async() => {
-      const params = { ...userFrom, ...pager }
-      const { list, page } = await userListApi(params)
-      pager.pageNum = page.pageNum
-      pager.total = page.total
-      userList.value = list
-    }
-    const getAllUsersList = async() => {
-      const params = { ...pager }
-      const { list, page } = await userAllListApi(params)
-      pager.pageNum = page.pageNum
-      pager.total = page.total
-      userList.value = list
-      console.log(userList.value)
-    }
-    const deleteUser = async() => {
-      if (userSelects.value.length > 0)
-        return deleteUserApi({ userIds: userSelects.value })
-      else
-        proxy.$message.error('请选择删除项')
-    }
-    const getRoleList = async() => {
-      roleList.value = await rolesAllListApi()
-    }
-    const addUser = async() => {
-      const userFormRaw = toRaw(addUserFrom)
-      return addUserApi(userFormRaw)
-    }
-    const editUser = async() => {
-      const userFormRaw = toRaw(addUserFrom)
-      return editUserApi(userFormRaw)
-    }
-    // 通用方法
-    const resetFields = (refName) => {
-      proxy.$refs[refName].resetFields()
-    }
-    // 事件方法: 多选时存入选中列表中
-    const onChangeUserSelects = (list) => {
-      userSelects.value = list.map(user => user.userId)
-    }
-    const onChangeCurrentPage = (currentPage) => {
-      pager.pageNum = currentPage
+
+const { proxy } = getCurrentInstance()
+// 属性
+const userFrom = reactive({
+  userId: '',
+  userName: '',
+  state: 1,
+})
+const pager = reactive({
+  pageNum: 1,
+  pageSize: 10,
+  total: 0,
+})
+const userColumns = [
+  { prop: 'userId', label: '用户ID' },
+  { prop: 'userName', label: '用户名' },
+  {
+    prop: 'sex',
+    label: '性别',
+    formatter(row, column, cellValue) {
+      return { 0: '女', 1: '男' }[cellValue]
+    },
+  },
+  { prop: 'userEmail', label: '邮箱' },
+  { prop: 'mobile', label: '手机号' },
+  {
+    prop: 'role',
+    label: '角色',
+    formatter(row, column, cellValue) {
+      return { 0: '管理员', 1: '普通用户', 2: '游客' }[cellValue]
+    },
+  },
+  {
+    prop: 'state',
+    label: '状态',
+    formatter(row, column, cellValue) {
+      return { 0: '已注销', 1: '正常' }[cellValue]
+    },
+  },
+  {
+    prop: 'createTime',
+    label: '注册时间',
+    formatter(row, column, cellValue) {
+      return util.formateDate(new Date(cellValue))
+    },
+  },
+  {
+    prop: 'lastLoginTime',
+    label: '最后登录',
+    formatter(row, column, cellValue) {
+      return util.formateDate(new Date(cellValue))
+    },
+  },
+]
+const isEdit = ref(false)
+const userList = ref([])
+const userSelects = ref([])
+const addDialog = ref(false)
+const deleteDialog = ref(false)
+const addUserFrom = reactive({})
+const roleList = ref([])
+const deptList = ref([])
+
+const checkRepeatUserName = async(rule, value, callback) => {
+  const { userName } = addUserFrom.userName
+  if (!userName)
+    return
+  const { isRepeat } = await checkRepeatApi({ userName })
+  // return isRepeat !== undefined ? callback(new Error('邮箱已存在')) : callback()
+  if (isRepeat)
+    return callback(new Error('用户名已存在'))
+  return callback()
+}
+const checkRepeatUserEmail = async(rule, value, callback) => {
+  const { userEmail } = addUserFrom.userEmail
+  if (!userEmail)
+    return
+  const { isRepeat } = await checkRepeatApi({ userEmail })
+  // return isRepeat !== undefined ? callback(new Error('邮箱已存在')) : callback()
+  if (isRepeat)
+    return callback(new Error('邮箱已存在'))
+  return callback()
+}
+const checkRepeatMobile = async(rule, value, callback) => {
+  const { mobile } = addUserFrom.mobile
+  if (!mobile)
+    return
+  const { isRepeat } = await checkRepeatApi({ mobile })
+  // return isRepeat !== undefined ? callback(new Error('手机号已存在')) : callback()
+  if (isRepeat)
+    return callback(new Error('手机号已存在'))
+  return callback()
+}
+const addUserFromRules = {
+  userName: [{
+    required: true,
+    message: '必须填写用户名',
+    trigger: 'blur',
+  }, {
+    min: 3,
+    max: 18,
+    message: '长度在 3 到 18 个字符',
+    trigger: 'blur',
+  }, { validator: checkRepeatUserName, trigger: 'blur' },
+  { pattern: /^[a-zA-Z]+$/, message: '只能输入字母', trigger: 'blur' },
+  ],
+  userEmail: [{
+    required: true,
+    message: '必须填写邮箱',
+    trigger: 'blur',
+  }, {
+    type: 'email',
+    message: '邮箱格式不正确',
+    trigger: 'blur',
+  },
+  { validator: checkRepeatUserEmail, trigger: 'blur' },
+  ],
+  mobile: [{
+    pattern: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
+    message: '手机号格式错误',
+    trigger: 'blur',
+  }, {
+    type: 'number',
+    message: '手机号格式错误',
+    trigger: 'blur',
+  }, {
+    min: 11,
+    max: 11,
+    message: '手机号长度错误',
+    trigger: 'blur',
+  }, { validator: checkRepeatMobile, trigger: 'blur' }],
+}
+
+const getUserList = async() => {
+  const params = { ...userFrom, ...pager }
+  const { list, page } = await userListApi(params)
+  pager.pageNum = page.pageNum
+  pager.total = page.total
+  userList.value = list
+}
+const getAllUsersList = async() => {
+  const params = { ...pager }
+  const { list, page } = await userAllListApi(params)
+  pager.pageNum = page.pageNum
+  pager.total = page.total
+  userList.value = list
+  console.log(userList.value)
+}
+const deleteUser = async() => {
+  if (userSelects.value.length > 0)
+    return deleteUserApi({ userIds: userSelects.value })
+  else
+    proxy.$message.error('请选择删除项')
+}
+const getRoleList = async() => {
+  roleList.value = await rolesAllListApi()
+}
+const addUser = async() => {
+  const userFormRaw = toRaw(addUserFrom)
+  return addUserApi(userFormRaw)
+}
+const editUser = async() => {
+  const userFormRaw = toRaw(addUserFrom)
+  return editUserApi(userFormRaw)
+}
+// 通用方法
+const resetFields = (refName) => {
+  proxy.$refs[refName].resetFields()
+}
+// 事件方法: 多选时存入选中列表中
+const onChangeUserSelects = (list) => {
+  userSelects.value = list.map(user => user.userId)
+}
+const onChangeCurrentPage = (currentPage) => {
+  pager.pageNum = currentPage
+  getUserList()
+}
+const onSearchUserFrom = () => {
+  getUserList()
+}
+const onResetUserFrom = () => {
+  proxy.$refs.formRef.resetFields()
+  getAllUsersList()
+}
+const onEditUser = async(user) => {
+  addDialog.value = true
+  isEdit.value = true
+  await nextTick(() => {
+    Object.assign(addUserFrom, user)
+  })
+}
+const onAddUserBtn = () => {
+  isEdit.value = false
+  addDialog.value = true
+}
+const onAddDeleteList = (user) => {
+  userSelects.value = [user.userId]
+  console.log('userSelects.value=>', userSelects.value)
+  deleteDialog.value = true
+}
+const onDeleteUserSelects = async() => {
+  try {
+    const { nModified } = await deleteUser()
+    if (nModified > 0) {
+      userSelects.value = []
+      proxy.$message.success('删除成功')
       getUserList()
     }
-    const onSearchUserFrom = () => {
-      getUserList()
+    else {
+      proxy.$message.error('删除失败')
     }
-    const onResetUserFrom = () => {
-      proxy.$refs.formRef.resetFields()
-      getAllUsersList()
-    }
-    const onEditUser = async(user) => {
-      addDialog.value = true
-      isEdit.value = true
-      await nextTick(() => {
-        Object.assign(addUserFrom, user)
+  }
+  catch (error) {}
+  deleteDialog.value = false
+}
+const onCancel = () => {
+  isEdit.value = false
+  resetFields('addFromRef')
+  addDialog.value = false
+}
+const resetPassword = () => {
+  console.log('addUserFrom=>', addUserFrom)
+  ElMessageBox.confirm('确认重置密码?', 'Warning', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  })
+    .then(() => {
+      addUserFrom.changeAction = 'reset'
+      changePasswordApi(addUserFrom)
+      ElMessage({
+        type: 'success',
+        message: '重置成功',
       })
-    }
-    const onAddUserBtn = () => {
-      isEdit.value = false
-      addDialog.value = true
-    }
-    const onAddDeleteList = (user) => {
-      userSelects.value = [user.userId]
-      console.log('userSelects.value=>', userSelects.value)
-      deleteDialog.value = true
-    }
-    const onDeleteUserSelects = async() => {
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: '取消重置',
+      })
+    })
+}
+const onSummit = () => {
+  proxy.$refs.addFromRef.validate(async(valid) => {
+    if (valid) {
       try {
-        const { nModified } = await deleteUser()
-        if (nModified > 0) {
-          userSelects.value = []
-          proxy.$message.success('删除成功')
-          getUserList()
-        }
-        else {
-          proxy.$message.error('删除失败')
-        }
+        let res
+        if (isEdit.value)
+          res = await editUser()
+        else
+          res = await addUser()
+
+        if (res)
+          proxy.$message.success('用户添加成功')
+        else
+          proxy.$message.error('用户添加失败')
+
+        resetFields('addFromRef')
       }
       catch (error) {}
-      deleteDialog.value = false
-    }
-    const onCancel = () => {
-      isEdit.value = false
-      resetFields('addFromRef')
+      getAllUsersList()
       addDialog.value = false
     }
-    const resetPassword = () => {
-      console.log('addUserFrom=>', addUserFrom)
-      ElMessageBox.confirm('确认重置密码?', 'Warning', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning',
-      })
-        .then(() => {
-          addUserFrom.changeAction = 'reset'
-          changePasswordApi(addUserFrom)
-          ElMessage({
-            type: 'success',
-            message: '重置成功',
-          })
-        })
-        .catch(() => {
-          ElMessage({
-            type: 'info',
-            message: '取消重置',
-          })
-        })
-    }
-    const onSummit = () => {
-      proxy.$refs.addFromRef.validate(async(valid) => {
-        if (valid) {
-          try {
-            let res
-            if (isEdit.value)
-              res = await editUser()
-            else
-              res = await addUser()
-
-            if (res)
-              proxy.$message.success('用户添加成功')
-            else
-              proxy.$message.error('用户添加失败')
-
-            resetFields('addFromRef')
-          }
-          catch (error) {}
-          getAllUsersList()
-          addDialog.value = false
-        }
-      })
-    }
-    // 生命周期
-    onMounted(() => {
-      getRoleList()
-      getAllUsersList()
-    })
-    //
-    return {
-      userFrom,
-      userColumns,
-      userList,
-      onChangeUserSelects,
-      pager,
-      isEdit,
-      addUserFrom,
-      addUserFromRules,
-      roleList,
-      deptList,
-      addDialog,
-      deleteDialog,
-      onChangeCurrentPage,
-      onSearchUserFrom,
-      onResetUserFrom,
-      onEditUser,
-      onAddUserBtn,
-      onAddDeleteList,
-      onDeleteUserSelects,
-      resetPassword,
-      onSummit,
-      onCancel,
-    }
-  },
+  })
+}
+// 生命周期
+onMounted(() => {
+  getRoleList()
+  getAllUsersList()
 })
+//
 </script>
 <style lang="scss" scoped>
   .user-page {
