72a74f55429a19c1313548d014218c7cee05b30a
laiczhang <i@laiczhang.com>
2022-04-02 22:23:16 +0800

    feat: update the rules of adding pusher form

--- src/views/Pusher.vue
+++ src/views/Pusher.vue
@@ -169,7 +169,7 @@
           <el-input v-model="addPusherFrom.pushLocation" disabled />
         </el-form-item>
         <el-form-item label="推送标题" prop="pusherTitle">
-          <el-input v-model="addPusherFrom.pusherTitle" placeholder="请输入推送标题" />
+          <el-input v-model="addPusherFrom.pusherTitle" placeholder="请输入推送标题，不填则使用推送类型作为标题" />
         </el-form-item>
         <!-- <el-form-item label="推送内容" prop="pusherContent">
           <el-input
@@ -189,7 +189,7 @@
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="推送周期">
+        <el-form-item label="推送周期" prop="pusherLifeTime">
           <el-select v-model="addPusherFrom.pusherLifeTime" placeholder="请选择推送周期">
             <el-option
               v-for="item in pushLifetimeOptions"
@@ -222,9 +222,7 @@
   </div>
 </template>
 
-<script>
-// import baseCountDown from '../components/countDown/baseCountDown.vue';
-// import countDownTime from '../components/countDown/countDownTime.vue';
+<script setup>
 import {
   defineComponent,
   getCurrentInstance,
@@ -248,337 +246,301 @@ import {
 import { getDictApi } from '../api/dict'
 import util from '../util/utils'
 import storage from '@/util/storage'
-export default defineComponent({
-  name: 'Pusher',
-  // components: { baseCountDown, countDownTime },
-  setup() {
-    const { proxy } = getCurrentInstance()
-    // 属性
-    const userInfo = storage.getItem('userInfo')
-    const pusherFrom = reactive({
-      userId: userInfo.userId,
-      pusherTitle: '',
-      pusherContent: '',
-      // pusherCategoryOptions: undefined
-    })
-    const shortcuts = [
-      {
-        text: '此刻',
-        value: new Date(),
-      },
-      {
-        text: '1小时后',
-        value: () => {
-          const date = new Date()
-          date.setTime(date.getTime() + 3600 * 1000)
-          return date
-        },
-      },
-      {
-        text: '3小时后',
-        value: () => {
-          const date = new Date()
-          date.setTime(date.getTime() + 3600 * 1000 * 3)
-          return date
-        },
-      },
-      {
-        text: '1天后',
-        value: () => {
-          const date = new Date()
-          date.setTime(date.getTime() + 3600 * 1000 * 24)
-          return date
-        },
-      },
-      {
-        text: '3天后',
-        value: () => {
-          const date = new Date()
-          date.setTime(date.getTime() + 3600 * 1000 * 24 * 3)
-          return date
-        },
-      },
-    ]
-    const pager = reactive({
-      pageNum: 1,
-      pageSize: 10,
-      total: 0,
-    })
-    const pusherColumns = [
-      { prop: 'pusherId', label: '推送ID' },
-      { prop: 'userId', label: '用户ID' },
-      { prop: 'userName', label: '用户名' },
-      { prop: 'pusherTitle', label: '推送标题' },
-      // {
-      //   prop: 'pusherContent',
-      //   label: '推送内容',
-      // },
-      {
-        prop: 'pusherCategory',
-        label: '推送类型',
-        formatter(row, column, cellValue) {
-          return { 0: '普通天气预报', 1: '降雨提醒', 2: '晴天提醒', 3: '穿衣指数提醒' }[cellValue]
-        },
-      },
-      {
-        prop: 'pusherLifeTime',
-        label: '推送周期',
-        formatter(row, column, cellValue) {
-          return { 0: '每天', 1: '一次' }[cellValue]
-        },
-      },
-      {
-        prop: 'state',
-        label: '推送状态',
-        formatter(row, column, cellValue) {
-          return { 0: '待推送', 1: '今日已推送', 2: '已发送/已过期' }[cellValue]
-        },
-      },
-      {
-        prop: 'pushTime',
-        label: '推送时间',
-        formatter(row, column, cellValue) {
-          return util.formateDate(new Date(cellValue))
-        },
-      },
-      {
-        prop: 'createTime',
-        label: '创建时间',
-        formatter(row, column, cellValue) {
-          return util.formateDate(new Date(cellValue))
-        },
-      },
-      // {
-      //   prop: 'updateTime',
-      //   label: '更新时间',
-      //   formatter(row, column, cellValue) {
-      //     return util.formateDate(new Date(cellValue));
-      //   },
-      // },
-    ]
-    const isEdit = ref(false)
-    const pusherList = ref([])
-    const pusherSelects = ref([])
-    const addDialog = ref(false)
-    const deleteDialog = ref(false)
-    const moreDialog = ref(false)
-    const immediatelyPushDialog = ref(false)
-    const addPusherFrom = reactive({})
-    addPusherFrom.userId = userInfo.userId
-    addPusherFrom.userName = userInfo.userName
-    const roleList = ref([])
-    const deptList = ref([])
-    const pickerOptions = {
-      disabledDate(time) {
-        return time.getTime() < Date.now() - 3600 * 1000 * 24
-      },
-    }
-    const addPusherFromRules = {
-      pusherTitle: {
-        required: true,
-        message: '必须填写推送标题',
-        trigger: 'blur',
-      },
-      pusherContent: {
-        required: true,
-        message: '必须选择推送类型',
-        trigger: 'blur',
-      },
-    }
-    // api
-    const getPusherList = async() => {
-      const params = { ...pusherFrom, ...pager }
-      const { list, page } = await pusherListApi(params)
-      pager.pageNum = page.pageNum
-      pager.total = page.total
-      pusherList.value = list
-    }
-    const getAllPushersList = async() => {
-      const { list, page } = await pusherAllListApi({ userId: userInfo.userId })
-      pager.pageNum = page.pageNum
-      pager.total = page.total
-      pusherList.value = list
-    }
-    const pusherCategoryOptions = ref([])
-    const getPusherCategoryOptions = async() => {
-      pusherCategoryOptions.value = await getDictApi('pusher_category')
-      console.log('pusherCategoryOptions.value=>', pusherCategoryOptions.value)
-    }
-    const pushLifetimeOptions = ref([])
-    const getPushLifetimeOptions = async() => {
-      pushLifetimeOptions.value = await getDictApi('pusher_lifetime')
-      console.log('pushLifetimeOptions.value=>', pushLifetimeOptions.value)
-    }
-    const deletePusher = async() => {
-      if (pusherSelects.value.length > 0)
-        return deletePusherApi({ _ids: pusherSelects.value })
-      else
-        proxy.$message.error('请选择删除项')
 
-      pusherAllListApi()
+const { proxy } = getCurrentInstance()
+// 属性
+const userInfo = storage.getItem('userInfo')
+const pusherFrom = reactive({
+  userId: userInfo.userId,
+  pusherTitle: '',
+  pusherContent: '',
+  // pusherCategoryOptions: undefined
+})
+const shortcuts = [
+  {
+    text: '此刻',
+    value: new Date(),
+  },
+  {
+    text: '1小时后',
+    value: () => {
+      const date = new Date()
+      date.setTime(date.getTime() + 3600 * 1000)
+      return date
+    },
+  },
+  {
+    text: '3小时后',
+    value: () => {
+      const date = new Date()
+      date.setTime(date.getTime() + 3600 * 1000 * 3)
+      return date
+    },
+  },
+  {
+    text: '1天后',
+    value: () => {
+      const date = new Date()
+      date.setTime(date.getTime() + 3600 * 1000 * 24)
+      return date
+    },
+  },
+  {
+    text: '3天后',
+    value: () => {
+      const date = new Date()
+      date.setTime(date.getTime() + 3600 * 1000 * 24 * 3)
+      return date
+    },
+  },
+]
+const pager = reactive({
+  pageNum: 1,
+  pageSize: 10,
+  total: 0,
+})
+const pusherColumns = [
+  { prop: 'pusherId', label: '推送ID' },
+  { prop: 'userId', label: '用户ID' },
+  { prop: 'userName', label: '用户名' },
+  { prop: 'pusherTitle', label: '推送标题' },
+  // {
+  //   prop: 'pusherContent',
+  //   label: '推送内容',
+  // },
+  {
+    prop: 'pusherCategory',
+    label: '推送类型',
+    formatter(row, column, cellValue) {
+      return { 0: '普通天气预报', 1: '降雨提醒', 2: '晴天提醒', 3: '穿衣指数提醒' }[cellValue]
+    },
+  },
+  {
+    prop: 'pusherLifeTime',
+    label: '推送周期',
+    formatter(row, column, cellValue) {
+      return { 0: '每天', 1: '一次' }[cellValue]
+    },
+  },
+  {
+    prop: 'state',
+    label: '推送状态',
+    formatter(row, column, cellValue) {
+      return { 0: '待推送', 1: '今日已推送', 2: '已发送/已过期' }[cellValue]
+    },
+  },
+  {
+    prop: 'pushTime',
+    label: '推送时间',
+    formatter(row, column, cellValue) {
+      return util.formateDate(new Date(cellValue))
+    },
+  },
+  {
+    prop: 'createTime',
+    label: '创建时间',
+    formatter(row, column, cellValue) {
+      return util.formateDate(new Date(cellValue))
+    },
+  },
+  // {
+  //   prop: 'updateTime',
+  //   label: '更新时间',
+  //   formatter(row, column, cellValue) {
+  //     return util.formateDate(new Date(cellValue));
+  //   },
+  // },
+]
+const isEdit = ref(false)
+const pusherList = ref([])
+const pusherSelects = ref([])
+const addDialog = ref(false)
+const deleteDialog = ref(false)
+const moreDialog = ref(false)
+const immediatelyPushDialog = ref(false)
+const addPusherFrom = reactive({})
+addPusherFrom.userId = userInfo.userId
+addPusherFrom.userName = userInfo.userName
+const roleList = ref([])
+const deptList = ref([])
+const pickerOptions = {
+  disabledDate(time) {
+    return time.getTime() < Date.now() - 3600 * 1000 * 24
+  },
+}
+const addPusherFromRules = {
+  // pusherTitle: {
+  //   required: true,
+  //   message: '必须填写推送标题',
+  //   trigger: 'blur',
+  // },
+  pusherCategory: {
+    required: true,
+    message: '必须选择推送类型',
+    trigger: 'blur',
+  },
+  pusherLifeTime: {
+    required: true,
+    message: '必须选择推送类型',
+    trigger: 'blur',
+  },
+}
+// api
+const getPusherList = async() => {
+  const params = { ...pusherFrom, ...pager }
+  const { list, page } = await pusherListApi(params)
+  pager.pageNum = page.pageNum
+  pager.total = page.total
+  pusherList.value = list
+}
+const getAllPushersList = async() => {
+  const { list, page } = await pusherAllListApi({ userId: userInfo.userId })
+  pager.pageNum = page.pageNum
+  pager.total = page.total
+  pusherList.value = list
+}
+const pusherCategoryOptions = ref([])
+const getPusherCategoryOptions = async() => {
+  pusherCategoryOptions.value = await getDictApi('pusher_category')
+  console.log('pusherCategoryOptions.value=>', pusherCategoryOptions.value)
+}
+const pushLifetimeOptions = ref([])
+const getPushLifetimeOptions = async() => {
+  pushLifetimeOptions.value = await getDictApi('pusher_lifetime')
+  console.log('pushLifetimeOptions.value=>', pushLifetimeOptions.value)
+}
+const deletePusher = async() => {
+  if (pusherSelects.value.length > 0)
+    return deletePusherApi({ _ids: pusherSelects.value })
+  else
+    proxy.$message.error('请选择删除项')
+
+  pusherAllListApi()
+}
+const getRoleList = async() => {
+  roleList.value = await rolesAllListApi()
+}
+const addPusher = async() => {
+  const pusherFormRaw = toRaw(addPusherFrom)
+  console.log('pusherFormRaw', pusherFormRaw)
+  return addPusherApi(pusherFormRaw)
+}
+const editPusher = async() => {
+  const pusherFormRaw = toRaw(addPusherFrom)
+  return editPusherApi(pusherFormRaw)
+}
+// 通用方法
+const resetFields = (refName) => {
+  proxy.$refs[refName].resetFields()
+}
+// 事件方法: 多选时存入选中列表中
+const onChangePusherSelects = (list) => {
+  pusherSelects.value = list.map(pusher => pusher._id)
+}
+const onChangeCurrentPage = (currentPage) => {
+  pager.pageNum = currentPage
+  getPusherList()
+}
+const onSearchPusherFrom = () => {
+  getPusherList()
+}
+const onResetPusherFrom = () => {
+  proxy.$refs.formRef.resetFields()
+  getAllPushersList()
+}
+const onEditPusher = async(pusher) => {
+  addDialog.value = true
+  isEdit.value = true
+  await nextTick(() => {
+    Object.assign(addPusherFrom, pusher)
+  })
+}
+const onAddPusherBtn = async() => {
+  let ip = ''
+  if (!ip) {
+    ip = await getIpApi()
+    addPusherFrom.pushIp = ip.ip
+    const { location } = await getLocationApi()
+    addPusherFrom.pushLocation = location.province + location.city
+  }
+  console.log('addPusherFrom=>', addPusherFrom)
+  isEdit.value = false
+  addDialog.value = true
+}
+const onAddDeleteList = (pusher) => {
+  pusherSelects.value = [pusher._id]
+  console.log('pusherSelects.value=>', pusherSelects.value)
+  deleteDialog.value = true
+}
+const onDeletePusherSelects = async() => {
+  try {
+    const { nModified } = await deletePusher()
+    if (nModified > 0) {
+      pusherSelects.value = []
+      proxy.$message.success('删除成功')
     }
-    const getRoleList = async() => {
-      roleList.value = await rolesAllListApi()
-    }
-    const addPusher = async() => {
-      const pusherFormRaw = toRaw(addPusherFrom)
-      console.log('pusherFormRaw', pusherFormRaw)
-      return addPusherApi(pusherFormRaw)
-    }
-    const editPusher = async() => {
-      const pusherFormRaw = toRaw(addPusherFrom)
-      return editPusherApi(pusherFormRaw)
-    }
-    // 通用方法
-    const resetFields = (refName) => {
-      proxy.$refs[refName].resetFields()
-    }
-    // 事件方法: 多选时存入选中列表中
-    const onChangePusherSelects = (list) => {
-      pusherSelects.value = list.map(pusher => pusher._id)
-    }
-    const onChangeCurrentPage = (currentPage) => {
-      pager.pageNum = currentPage
-      getPusherList()
-    }
-    const onSearchPusherFrom = () => {
-      getPusherList()
-    }
-    const onResetPusherFrom = () => {
-      proxy.$refs.formRef.resetFields()
-      getAllPushersList()
-    }
-    const onEditPusher = async(pusher) => {
-      addDialog.value = true
-      isEdit.value = true
-      await nextTick(() => {
-        Object.assign(addPusherFrom, pusher)
-      })
-    }
-    const onAddPusherBtn = async() => {
-      let ip = ''
-      if (!ip) {
-        ip = await getIpApi()
-        addPusherFrom.pushIp = ip.ip
-        const { location } = await getLocationApi()
-        addPusherFrom.pushLocation = location.province + location.city
-      }
-      console.log('addPusherFrom=>', addPusherFrom)
-      isEdit.value = false
-      addDialog.value = true
-    }
-    const onAddDeleteList = (pusher) => {
-      pusherSelects.value = [pusher._id]
-      console.log('pusherSelects.value=>', pusherSelects.value)
-      deleteDialog.value = true
-    }
-    const onDeletePusherSelects = async() => {
+  }
+  catch (error) {}
+  deleteDialog.value = false
+  getAllPushersList()
+}
+const onCancel = () => {
+  isEdit.value = false
+  resetFields('addFromRef')
+  addDialog.value = false
+}
+const watchMore = (val) => {
+  // console.log('watchMore', val)
+  moreDialog.value = true
+}
+// let pushTime = ref('2022-03-20 9:51:00');
+let pusherLifeTime = ''
+const openImmediatelyPushDialog = (val) => {
+  // console.info('immediatelyPush', val)
+  _id = val._id
+  pusherId = val.pusherId
+  // pushTime = val.pushTime;
+  pusherLifeTime = val.pusherLifeTime
+  immediatelyPushDialog.value = true
+}
+const immediatelyPush = async() => {
+  await immediatelyPushApi({ _id, pusherId, userId: userInfo.userId, pusherLifeTime })
+  await getAllPushersList()
+  immediatelyPushDialog.value = false
+}
+const onSummit = () => {
+  proxy.$refs.addFromRef.validate(async(valid) => {
+    if (valid) {
       try {
-        const { nModified } = await deletePusher()
-        if (nModified > 0) {
-          pusherSelects.value = []
-          proxy.$message.success('删除成功')
-        }
+        let res
+        if (isEdit.value)
+          res = await editPusher()
+        else
+          res = await addPusher()
+
+        if (res)
+          proxy.$message.success('推送添加成功')
+        else
+          proxy.$message.error('推送添加失败')
+
+        resetFields('addFromRef')
       }
-      catch (error) {}
-      deleteDialog.value = false
+      catch (error) {
+        proxy.$message.error('推送添加失败')
+      }
       getAllPushersList()
-    }
-    const onCancel = () => {
-      isEdit.value = false
-      resetFields('addFromRef')
       addDialog.value = false
     }
-    const watchMore = (val) => {
-      // console.log('watchMore', val)
-      moreDialog.value = true
-    }
-    // let pushTime = ref('2022-03-20 9:51:00');
-    let pusherLifeTime = ''
-    const openImmediatelyPushDialog = (val) => {
-      // console.info('immediatelyPush', val)
-      _id = val._id
-      pusherId = val.pusherId
-      // pushTime = val.pushTime;
-      pusherLifeTime = val.pusherLifeTime
-      immediatelyPushDialog.value = true
-    }
-    const immediatelyPush = async() => {
-      await immediatelyPushApi({ _id, pusherId, userId: userInfo.userId, pusherLifeTime })
-      await getAllPushersList()
-      immediatelyPushDialog.value = false
-    }
-    const onSummit = () => {
-      proxy.$refs.addFromRef.validate(async(valid) => {
-        if (valid) {
-          try {
-            let res
-            if (isEdit.value)
-              res = await editPusher()
-            else
-              res = await addPusher()
-
-            if (res)
-              proxy.$message.success('推送操作成功')
-            else
-              proxy.$message.error('推送添加失败')
-
-            resetFields('addFromRef')
-          }
-          catch (error) {
-            proxy.$message.error('推送添加失败')
-          }
-          getAllPushersList()
-          addDialog.value = false
-        }
-      })
-    }
-    // 生命周期
-    onMounted(() => {
-      getPusherCategoryOptions()
-      getPushLifetimeOptions()
-      getRoleList()
-      getAllPushersList()
-    })
-    //
-    return {
-      pusherFrom,
-      pusherColumns,
-      pusherList,
-      pusherCategoryOptions,
-      shortcuts,
-      pickerOptions,
-      pushLifetimeOptions,
-      // pushTime,
-      // pusherLifetime,
-      getPushLifetimeOptions,
-      onChangePusherSelects,
-      getPusherCategoryOptions,
-      pager,
-      isEdit,
-      addPusherFrom,
-      addPusherFromRules,
-      roleList,
-      deptList,
-      addDialog,
-      immediatelyPushDialog,
-      deleteDialog,
-      moreDialog,
-      immediatelyPush,
-      openImmediatelyPushDialog,
-      onChangeCurrentPage,
-      onSearchPusherFrom,
-      onResetPusherFrom,
-      onEditPusher,
-      onAddPusherBtn,
-      onAddDeleteList,
-      onDeletePusherSelects,
-      watchMore,
-      onSummit,
-      onCancel,
-    }
-  },
+  })
+}
+// 生命周期
+onMounted(() => {
+  getPusherCategoryOptions()
+  getPushLifetimeOptions()
+  getRoleList()
+  getAllPushersList()
 })
+//
 </script>
 <style lang="scss" scoped>
   .pusher-page {
