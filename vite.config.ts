b8d4f14ccdc7e7b4b809a27cd1a8b1bbc273ac87
laiczhang <i@laiczhang.com>
2023-11-17 11:13:14 +0800

    chore: upgrade vite to 5

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,7 @@ else {
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    extensions: ['.mjs', '.js', '.cjs','.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
       '@': '/src',
     },
